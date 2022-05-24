4e4c7ef97adc6241cf0ad5282e141230476fd226
mayank190821 <bhugramayank@gmail.com>
2022-05-24 14:01:11 +0530

    style table

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -12,37 +12,43 @@ function Table() {
   }, []);
   return (
     <>
-      <table style={{ border: "1px solid red", width: "50vw" }}>
-        <thead>All Books</thead>
-        <tbody>
-          <tr>
+      <div className="tableContainer">
+        <table>
+          <caption style={{ captionSide: "top", textAlign: "center" }}>
+            All Books
+          </caption>
+          <tr
+            style={{ background: "red", color: "white", textAlign: "center" }}
+          >
             <th>S.No.</th>
             <th>Book Name</th>
             <th>Author</th>
             <th>Price</th>
           </tr>
-          {(tableData && tableData.length !== 0 )? (
-            <div>
-              {tableData.map((elem, indx) => {
-                return (
-                  <tr>
-                    <td>{indx}</td>
-                    <td>{elem.bookName}</td>
-                    <td> {elem.authorName}</td>
-                    <td> {elem.bookPrice}</td>
-                    <td>
-                      <i className="fas fa-pen" />
-                      <i class="fas fa-times"></i>
-                    </td>
-                  </tr>
-                );
-              })}
-            </div>
-          ) : (
-            <tr />
-          )}
-        </tbody>
-      </table>
+          <tbody>
+            {tableData && tableData.length !== 0 ? (
+              <div>
+                {tableData.map((elem, indx) => {
+                  return (
+                    <tr>
+                      <td>{indx}</td>
+                      <td>{elem.bookName}</td>
+                      <td> {elem.authorName}</td>
+                      <td> {elem.bookPrice}</td>
+                      <td>
+                        <i className="fas fa-pen" />
+                        <i class="fas fa-times"></i>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </div>
+            ) : (
+              <tr />
+            )}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
